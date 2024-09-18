60c5714b00da1e6487c03b47193557087f7220d5
Mikael Marques <110744151+mikaelmarques7@users.noreply.github.com>
2024-09-18 19:26:52 -0300

    Refatora código

--- script.js
+++ script.js
@@ -1,29 +1,65 @@
-const caixaDeSenha = document.getElementById("senha");
-const length = 12;
+const passwordBox = document.getElementById("senha");
+const inputLength = document.getElementById("length");
+const checkUppercase = document.getElementById("include-upper");
+const checkLowercase = document.getElementById("include-lower");
+const checkNumber = document.getElementById("include-number");
+const checkSymbols = document.getElementById("include-symbols");
+const messageBox = document.getElementById("message");
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const numberChars = "0123456789";
+const symbolChars = "@#$%^&*()_+~|}{[]></-=";
 
-const number = "0123456789";
-const symbol = "@#$%^&*()_+~|}{[]></-=";
 
-const todosOsCaractrs = upperCase + lowerCase + number + symbol;
 
-function criarSenha() {
-    let senha = "";
-    senha += upperCase[Math.floor(Math.random()* upperCase.length)];
-    senha += lowerCase[Math.floor(Math.random()* lowerCase.length)];
-    senha += number[Math.floor(Math.random()* number.length)];
-    senha += symbol[Math.floor(Math.random()* symbol.length)];
+function generatePassword() {
+    let allCharacters = "";
+    let password = "";
+    const length = parseInt(inputLength.value) || 12;
 
-    while(length > senha.length){
-        senha += todosOsCaractrs[Math.floor(Math.random()* todosOsCaractrs.length)];
+
+    if (!checkUppercase.checked && !checkLowercase.checked && !checkNumber.check && !checkSymbols.check) {
+        alert("Selecione ao menos uma opção de caracteres para gerar uma senha ");
+        return;
     }
-    caixaDeSenha.value = senha;
+
+    if (checkUppercase.checked) {
+        allCharacters += uppercaseChars;
+        password += uppercaseChars[Math.floor(Math.random() * uppercaseChars.length)];
+    }
+
+    if (checkLowercase.checked) {
+        allCharacters += lowercaseChars;
+        password += lowercaseChars[Math.floor(Math.random() * lowercaseChars.length)];
+    }
+
+
+    if (checkNumber.checked) {
+        allCharacters += numberChars;
+        password += numberChars[Math.floor(Math.random() * numberChars.length)];
+    }
+
+
+    if (checkSymbols.checked) {
+        allCharacters += symbolChars;
+        password += symbolChars[Math.floor(Math.random() * symbolChars.length)];
+    }
+
+    while (password.length < length) {
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length)]
+    }
+    passwordBox.value = password
 }
 
 
-function copiarSenha() {
-    caixaDeSenha.select();
-    document.execCommand("copy")
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+
+    messageBox.classList.add("show");
+
+    setTimeout(() => {
+        messageBox.classList.remove("show")
+    }, 1000)
 }
